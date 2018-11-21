4799c62421c7833e8f3fa9062274e68b4aeb8b7b
eskimo-ben <ben@2eskimos.com>
2018-11-21 14:46:17 +0000

    Working selector

---
+++ FootballApp/Menu.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FootballApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            Data.Load();
+            InitializeComponent();
+        }
+
+        private void b_Fixtures_Click(object sender, RoutedEventArgs e)
+        {
+            new Selector(new Game()).Show();
+        }
+
+        private void b_Teams_Click(object sender, RoutedEventArgs e)
+        {
+            new Selector(new Team()).Show();
+        }
+
+        private void b_Players_Click(object sender, RoutedEventArgs e)
+        {
+            new Selector(new Player()).Show();
+        }
+    }
+}
---
+++ FootballApp/Ui/Selector.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FootballApp
+{
+    /// <summary>
+    /// Interaction logic for GamesSelect.xaml
+    /// </summary>
+    public partial class Selector : Window
+    {
+        private Selector()
+        {
+            InitializeComponent();
+        }
+
+        public Selector(Game pGame):this()
+        {
+            Title = "Game Selector";
+            foreach (Game game in Data.games)
+            {
+                MainList.Items.Add(String.Format("{0} {1}",
+                    Data.teams.Single(team => team.teamCode == game.homeTeamCode),
+                    Data.teams.Single(team => team.teamCode == game.awayTeamCode)
+                ));
+            }
+        }
+
+        public Selector(Player pPlayer) : this()
+        {
+            Title = "Player Selector";
+            foreach(Player player in Data.players)
+            {
+                MainList.Items.Add(player.fullName);
+            }
+        }
+
+        public Selector(Team pTeam) : this()
+        {
+            Title = "Team Selector";
+            foreach (Team team in Data.teams)
+            {
+                MainList.Items.Add(team.name);
+            }
+        }
+    }
+}
